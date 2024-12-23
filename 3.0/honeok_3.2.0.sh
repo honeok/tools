d39c423d5db88c774337cfde771816d91049596b
honeok <yihaohey@gmail.com>
2024-12-23 09:57:16 +0800

    update

--- 3.0/honeok_3.2.0.sh
+++ 3.0/honeok_3.2.0.sh
@@ -11,7 +11,7 @@
 #       @teddysun   <https://github.com/teddysun>
 #       @spiritLHLS <https://github.com/spiritLHLS>
 
-honeok_v="v3.2.0 (2024.12.21)"
+honeok_v="v3.2.0 (2024.12.23)"
 
 yellow='\033[93m'
 red='\033[31m'
