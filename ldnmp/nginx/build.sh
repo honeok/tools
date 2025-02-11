da694535f0cc74cb088ee4219319f501ce44784a
honeok <yihaohey@gmail.com>
2025-02-11 22:19:03 +0800

    feat: prompts for version info

--- ldnmp/nginx/build.sh
+++ ldnmp/nginx/build.sh
@@ -26,13 +26,13 @@ CORERULESET_VERSION=""
 
 install() {
     if [ "$#" -eq 0 ]; then
-        echo "未提供软件包参数"
+        echo "ERROR: No package parameters provided!"
         return 1
     fi
 
     for package in "$@"; do
         if ! command -v "$package" >/dev/null 2>&1; then
-            echo "正在安装$package"
+            echo "INFO: Installing $package"
             if command -v dnf >/dev/null 2>&1; then
                 dnf install -y epel-release
                 dnf install -y "$package"
@@ -44,11 +44,11 @@ install() {
             elif command -v apt-get >/dev/null 2>&1; then
                 apt-get install -y "$package"
             else
-                echo "未知的包管理器！"
+                echo "ERROR: Unknown package manager!"
                 return 1
             fi
         else
-            echo "${package}已经安装！"
+            echo "INFO: $package is already installed!"
         fi
     done
     return 0
@@ -56,12 +56,10 @@ install() {
 
 install jq curl
 
-# 获取最新版本
-LATEST_NGINX_VERSION=$(curl -s https://api.github.com/repos/nginx/nginx/releases/latest | grep -o '"tag_name": "[^"]*' | awk -F'"' '{print $4}' | sed 's/release-//')
-ZSTD_VERSION=$(curl -s https://api.github.com/repos/facebook/zstd/releases/latest | grep -o '"tag_name": "[^"]*' | awk -F'"' '{print $4}' | sed 's/^v//')
-CORERULESET_VERSION=$(curl -s https://api.github.com/repos/coreruleset/coreruleset/releases/latest | grep -o '"tag_name": "[^"]*' | awk -F'"' '{print $4}' | sed 's/^v//')
+LATEST_NGINX_VERSION=$(curl -s https://api.github.com/repos/nginx/nginx/releases/latest | jq -r '.tag_name | sub("release-"; "")')
+ZSTD_VERSION=$(curl -s https://api.github.com/repos/facebook/zstd/releases/latest | jq -r '.tag_name | sub("^v"; "")')
+CORERULESET_VERSION=$(curl -s https://api.github.com/repos/coreruleset/coreruleset/releases/latest | jq -r '.tag_name | sub("^v"; "")')
 
-# 验证获取的版本是否有效
 for version in "$LATEST_NGINX_VERSION" "$ZSTD_VERSION" "$CORERULESET_VERSION"; do
     if [[ ! "$version" =~ ^[0-9]+\.[0-9]+(\.[0-9]+)?$ ]]; then
         echo "Invalid version format: $version" >&2
@@ -69,7 +67,10 @@ for version in "$LATEST_NGINX_VERSION" "$ZSTD_VERSION" "$CORERULESET_VERSION"; d
     fi
 done
 
-# 输出为GitHub Actions环境变量
+echo "Latest NGINX version: $LATEST_NGINX_VERSION"
+echo "Latest ZSTD version: $ZSTD_VERSION"
+echo "Latest CORERULESET version: $CORERULESET_VERSION"
+
 {
     echo "LATEST_NGINX_VERSION=${LATEST_NGINX_VERSION}"
     echo "ZSTD_VERSION=${ZSTD_VERSION}"
