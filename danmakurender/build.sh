5390ab71334571d98b0b49e52cdab37062424106
honeok <yihaohey@gmail.com>
2025-05-12 17:24:51 +0800

    chore: remove biliup-rs installation matching logic

--- danmakurender/build.sh
+++ danmakurender/build.sh
@@ -33,32 +33,10 @@ if ! git clone --branch "$DANMAKU_TAG" https://github.com/SmallPeaches/DanmakuRe
     printf 'Error: Unable to obtain DanmakuRender source code!\n' >&2; exit 1
 fi
 
-BILIUPRS_VERSION=$(curl -fsL "https://api.github.com/repos/biliup/biliup-rs/releases/latest" | awk -F '["v]' '/tag_name/{print $5}')
-readonly BILIUPRS_VERSION
-[ -z "$BILIUPRS_VERSION" ] && { printf 'Error: Unable to obtain biliup-rs version!\n' >&2; exit 1; }
-
-case "$(uname -m)" in
-    x86_64 | amd64)
-        BILIUPRS_FRAMEWORK='x86_64'
-    ;;
-    armv8* | arm64 | aarch64)
-        BILIUPRS_FRAMEWORK='aarch64'
-    ;;
-    *)
-        printf "Error: unsupported architecture: %s\n" "$(uname -m)" >&2; exit 1
-    ;;
-esac
-
-if ! curl -fsL -O "https://github.com/biliup/biliup-rs/releases/download/v$BILIUPRS_VERSION/biliupR-v$BILIUPRS_VERSION-$BILIUPRS_FRAMEWORK-linux.tar.xz"; then
-    printf 'Error: Failed to download biliup-rs, please check the network!\n' >&2; exit 1
-fi
-
 # prepare running environment.
 mv -f DanmakuRender/* .
-rm -rf DanmakuRender
-tar xf "biliupR-v$BILIUPRS_VERSION-$BILIUPRS_FRAMEWORK-linux.tar.xz" --strip-components=1
-rm -f "biliupR-v$BILIUPRS_VERSION-$BILIUPRS_FRAMEWORK-linux.tar.xz"
 mv -f biliup tools/
+rm -rf DanmakuRender
 
 # clean up the extra docs files in danmakurender repository.
 rm -f Dockerfile -- *.md
