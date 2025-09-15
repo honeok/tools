c8daefd066e0db93eba7fcc425ed6c42885b1b2e
honeok <i@honeok.com>
2025-09-15 11:20:24 +0800

    feat: use golang.google.cn mirror, remove cdn validation, standardize naming
    Signed-off-by: honeok <i@honeok.com>

--- infra/go.sh
+++ infra/go.sh
@@ -1,121 +1,120 @@
 #!/usr/bin/env bash
 #
-# Description: This script is used to install or update to the latest go version.
+# Description: This script is used to automatically installs or updates Go to the latest version.
 #
 # Copyright (c) 2025 honeok <i@honeok.com>
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: Apache-2.0
 
-_red() { printf "\033[91m%s\033[0m\n" "$*"; }
-_green() { printf "\033[92m%s\033[0m\n" "$*"; }
-_yellow() { printf "\033[93m%s\033[0m\n" "$*"; }
-_err_msg() { printf "\033[41m\033[1mError\033[0m %s\n" "$*"; }
-_suc_msg() { printf "\033[42m\033[1mSuccess\033[0m %s\n" "$*"; }
-_info_msg() { printf "\033[43m\033[1mInfo\033[0m %s\n" "$*"; }
+set -eE
+
+_red() { printf "\033[91m%b\033[0m\n" "$*"; }
+_green() { printf "\033[92m%b\033[0m\n" "$*"; }
+_yellow() { printf "\033[93m%b\033[0m\n" "$*"; }
+_err_msg() { printf "\033[41m\033[1mError\033[0m %b\n" "$*"; }
+_suc_msg() { printf "\033[42m\033[1mSuccess\033[0m %b\n" "$*"; }
+_info_msg() { printf "\033[43m\033[1mInfo\033[0m %b\n" "$*"; }
+
+# 设置PATH环境变量
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
 
 # 各变量默认值
-GITHUB_PROXY='https://files.m.daocloud.io/'
-RANDOM_CHAR="$(head /dev/urandom | tr -dc 'A-Za-z0-9' | head -c 5)"
-TEMP_DIR="/tmp/go_$RANDOM_CHAR"
+TEMP_DIR="$(mktemp -d)"
 
 # 设置系统utf-8语言环境
 UTF8_LOCALE="$(locale -a 2>/dev/null | grep -iEm1 "UTF-8|utf8")"
 [ -n "$UTF8_LOCALE" ] && export LC_ALL="$UTF8_LOCALE" LANG="$UTF8_LOCALE" LANGUAGE="$UTF8_LOCALE"
 
-# 打印错误信息并退出
+trap 'rm -rf "${TEMP_DIR:?}" >/dev/null 2>&1' INT TERM EXIT
+
+clrscr() {
+    [ -t 1 ] && tput clear 2>/dev/null || echo -e "\033[2J\033[H" || clear
+}
+
 die() {
     _err_msg >&2 "$(_red "$@")"; exit 1
 }
 
-mkdir -p "$TEMP_DIR" >/dev/null 2>&1
-if [ "$(cd -P -- "$(dirname -- "$0")" && pwd -P)" != "$TEMP_DIR" ]; then
-    cd "$TEMP_DIR" >/dev/null 2>&1 || die "Can't access temporary working directory."
-fi
-
-# 终止信号捕获退出前清理操作
-_exit() {
-    local ERR_CODE="$?"
-    rm -rf "$TEMP_DIR" >/dev/null 2>&1
-    exit "$ERR_CODE"
-}
-
-# 终止信号捕获
-trap '_exit' SIGINT SIGQUIT SIGTERM EXIT
-
-clrscr() {
-    ([ -t 1 ] && tput clear 2>/dev/null) || echo -e "\033[2J\033[H" || clear
-}
+# 临时工作目录
+cd "$TEMP_DIR" >/dev/null 2>&1
 
 _exists() {
     local _CMD="$1"
-    if type "$_CMD" >/dev/null 2>&1; then return 0;
-    elif command -v "$_CMD" >/dev/null 2>&1; then return 0;
-    elif which "$_CMD" >/dev/null 2>&1; then return 0;
+    if type "$_CMD" >/dev/null 2>&1; then return;
+    elif command -v "$_CMD" >/dev/null 2>&1; then return;
+    elif which "$_CMD" >/dev/null 2>&1; then return;
     else return 1;
     fi
 }
 
-check_root() {
+curl() {
+    local RET
+    # 添加 --fail 不然404退出码也为0
+    # 32位cygwin已停止更新, 证书可能有问题, 添加 --insecure
+    # centos7 curl 不支持 --retry-connrefused --retry-all-errors 因此手动 retry
+    for ((i=1; i<=5; i++)); do
+        command curl --connect-timeout 10 --fail --insecure "$@"
+        RET=$?
+        if [ "$RET" -eq 0 ]; then
+            return
+        else
+            # 403 404 错误或达到重试次数
+            if [ "$RET" -eq 22 ] || [ "$i" -eq 5 ]; then
+                return "$RET"
+            fi
+            sleep 1
+        fi
+    done
+}
+
+isChina() {
+    if [ -z "$COUNTRY" ]; then
+        if ! COUNTRY="$(curl -Ls http://www.qualcomm.cn/cdn-cgi/trace | grep '^loc=' | cut -d= -f2 | grep .)"; then
+            die "Can not get location."
+        fi
+        echo 2>&1 "Location: $COUNTRY"
+    fi
+}
+
+checkRoot() {
     if [ "$EUID" -ne 0 ] || [ "$(id -ru)" -ne 0 ]; then
         die "This script must be run as root!"
     fi
 }
 
-check_cdn() {
-    local IPV4_ADDRESS IPV6_ADDRESS
-
-    # https://danwin1210.de/github-ipv6-proxy.php
-    ipv6_proxy() {
-        local -a HOST_ENTRIES
-        command cp -f /etc/hosts{,.bak}
-        HOST_ENTRIES=(
-            "2a01:4f8:c010:d56::2 github.com"
-            "2a01:4f8:c010:d56::3 api.github.com"
-            "2a01:4f8:c010:d56::4 codeload.github.com"
-            "2a01:4f8:c010:d56::6 ghcr.io"
-            "2a01:4f8:c010:d56::7 pkg.github.com npm.pkg.github.com maven.pkg.github.com nuget.pkg.github.com rubygems.pkg.github.com"
-            "2a01:4f8:c010:d56::8 uploads.github.com"
-            "2606:50c0:8000::133 objects.githubusercontent.com www.objects.githubusercontent.com release-assets.githubusercontent.com gist.githubusercontent.com repository-images.githubusercontent.com camo.githubusercontent.com private-user-images.githubusercontent.com avatars0.githubusercontent.com avatars1.githubusercontent.com avatars2.githubusercontent.com avatars3.githubusercontent.com cloud.githubusercontent.com desktop.githubusercontent.com support.github.com"
-            "2606:50c0:8000::154 support-assets.githubassets.com github.githubassets.com opengraph.githubassets.com github-registry-files.githubusercontent.com github-cloud.githubusercontent.com"
-        )
-        for ENTRY in "${HOST_ENTRIES[@]}"; do
-            echo "$ENTRY" >> /etc/hosts
-        done
-    }
-    # 备用 www.prologis.cn www.autodesk.com.cn www.keysight.com.cn
-    COUNTRY="$(curl --max-time 5 --insecure --location --silent "https://www.qualcomm.cn/cdn-cgi/trace" | grep -i '^loc=' | cut -d'=' -f2 | grep .)"
-    IPV4_ADDRESS="$(curl --max-time 5 --insecure --location --silent --ipv4 "https://www.qualcomm.cn/cdn-cgi/trace" | grep -i '^ip=' | cut -d'=' -f2 | grep .)"
-    IPV6_ADDRESS="$(curl --max-time 5 --insecure --location --silent --ipv6 "https://www.qualcomm.cn/cdn-cgi/trace" | grep -i '^ip=' | cut -d'=' -f2 | grep .)"
-    if [ "$COUNTRY" != "CN" ] && [ -n "$IPV4_ADDRESS" ]; then
-        unset GITHUB_PROXY
-    elif [ "$COUNTRY" != "CN" ] && [ -z "$IPV4_ADDRESS" ] && [ -n "$IPV6_ADDRESS" ]; then
-        ipv6_proxy
-    fi
-}
-
-install_go() {
-    local GO_WORKDIR GO_ENV GO_VER OS_ARCH
+goInstall() {
+    local GO_WORKDIR GO_ENV GO_MIRROR GO_VER OS_NAME OS_ARCH
 
     GO_WORKDIR="/usr/local/go"
     GO_ENV="/etc/profile.d/go.sh"
-    GO_VER="$(curl --retry 5 --insecure --location --silent "https://go.dev/dl/?mode=json" | awk '/version/ {print $2}' | sed -n '1s/.*"go\(.*\)".*/\1/p')"
+    OS_NAME="$(uname -s 2>/dev/null | sed 's/.*/\L&/')"
+
+    if isChina; then
+        GO_MIRROR="golang.google.cn"
+    else
+        GO_MIRROR="go.dev"
+    fi
+
+    GO_VER="$(curl -Ls "https://$GO_MIRROR/dl/?mode=json" | awk '/version/ {print $2}' | sed -n '1s/.*"go\(.*\)".*/\1/p')"
+
     case "$(uname -m)" in
-        i*86 | x86 ) OS_ARCH="386" ;;
-        x86_64 | amd64 ) OS_ARCH="amd64" ;;
-        armv6* ) OS_ARCH="armv6" ;;
-        arm64 | aarch64 ) OS_ARCH="arm64" ;;
-        * ) die "unsupported architecture: $(uname -m)" ;;
+        i*86|x86) OS_ARCH="386" ;;
+        x86_64|amd64) OS_ARCH="amd64" ;;
+        armv6*) OS_ARCH="armv6" ;;
+        arm64|aarch64) OS_ARCH="arm64" ;;
+        *) die "unsupported architecture: $(uname -m)" ;;
     esac
+
     _info_msg "$(_yellow "Start downloading the go install package.")"
-    if ! curl --location --remote-name "${GITHUB_PROXY}go.dev/dl/go$GO_VER.linux-$OS_ARCH.tar.gz"; then
+    if ! curl -LO "https://$GO_MIRROR/dl/go$GO_VER.$OS_NAME-$OS_ARCH.tar.gz"; then
         die "Failed to download go install package, please check the network!"
     fi
     rm -rf "$GO_WORKDIR" >/dev/null 2>&1
     rm -f "$GO_ENV" >/dev/null 2>&1
-    tar xzf "go$GO_VER.linux-$OS_ARCH.tar.gz" -C /usr/local
+    tar fxz "go$GO_VER.linux-$OS_ARCH.tar.gz" -C /usr/local
     tee "$GO_ENV" >/dev/null <<EOF
 #!/bin/sh
-# Copyright (c) 2025 honeok <honeok@disroot.org>
+# Copyright (c) 2025 honeok <i@honeok.com>
 
 # GoLang Environment
 export GOROOT=/usr/local/go
@@ -129,26 +128,23 @@ EOF
     chown -R "$USER":"$USER" "${GOPATH:?}"
 }
 
-install_check() {
+goInfo() {
     if _exists go >/dev/null 2>&1; then
         _suc_msg "$(_green "Go installed successfully!")"
-        go version
+        go version 2>&1
     else
         die "Go installation failed, please check the error message."
     fi
 }
 
-go_proxy() {
-    [ "$COUNTRY" = "CN" ] && go env -w GOPROXY=https://goproxy.cn,direct || return 0
+goProxy() {
+    if isChina; then
+        go env -w GOPROXY=https://goproxy.cn,direct
+    fi
 }
 
-# 全局参数 (1/2)
 clrscr
-check_root
-check_cdn
-
-# 主安装逻辑 (2/2)
-install_go
-clrscr
-install_check
-go_proxy
+checkRoot
+goInstall
+goInfo
+goProxy
