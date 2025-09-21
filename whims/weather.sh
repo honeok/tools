4de330d4689f0565c79326bb613940ee03efa84c
honeok <i@honeok.com>
2025-09-21 19:27:02 +0800

    feat: remove user agent to add robustness to curl
    Signed-off-by: honeok <i@honeok.com>

--- whims/weather.sh
+++ whims/weather.sh
@@ -2,15 +2,14 @@
 #
 # Description: This script is used to accept the user's connection ip from the command line and obtain the weather in the user's location.
 #
-# Copyright (c) 2025 honeok <honeok@disroot.org>
+# Copyright (c) 2025 honeok <i@honeok.com>
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# 当前脚本版本号
-readonly VERSION='v1.1.3 (2025.06.11)'
+set -eE
 
-# https://www.graalvm.org/latest/reference-manual/ruby/UTF8Locale
-export LANG=en_US.UTF-8
+# 当前脚本版本号
+readonly VERSION='v25.9.21'
 
 # 自定义彩色字体
 _red() { printf "\033[91m%b\033[0m\n" "$*"; }
@@ -18,32 +17,63 @@ _yellow() { printf "\033[93m%b\033[0m\n" "$*"; }
 _cyan() { printf "\033[96m%b\033[0m\n" "$*"; }
 _err_msg() { printf "\033[41m\033[1mError\033[0m %b\n" "$*"; }
 
-# 各变量默认值
-UA_BROWSER='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'
+# 设置PATH环境变量
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
 
-# curl默认参数
-declare -a CURL_OPTS=(--max-time 5 --retry 2 --retry-max-time 20)
+# 设置系统utf-8语言环境
+UTF8_LOCALE="$(locale -a 2>/dev/null | grep -iEm1 "UTF-8|utf8")"
+[ -n "$UTF8_LOCALE" ] && export LC_ALL="$UTF8_LOCALE" LANG="$UTF8_LOCALE" LANGUAGE="$UTF8_LOCALE"
 
-# 清屏函数
-clrscr() {
-    ([ -t 1 ] && tput clear 2>/dev/null) || echo -e "\033[2J\033[H" || clear
+clear() {
+    [ -t 1 ] && tput clear 2>/dev/null || echo -e "\033[2J\033[H" || clear
 }
 
-# 打印错误信息并退出
 die() {
     _err_msg >&2 "$(_red "$@")"; exit 1
 }
 
-before_run() {
-    if [ ! -t 1 ]; then
-        die "This script requires a terminal environment."
-    fi
+curl() {
+    local RET
+    # 添加 --fail 不然404退出码也为0
+    # 32位cygwin已停止更新, 证书可能有问题, 添加 --insecure
+    # centos7 curl 不支持 --retry-connrefused --retry-all-errors 因此手动 retry
+    for ((i=1; i<=5; i++)); do
+        command curl --connect-timeout 10 --fail --insecure "$@"
+        RET=$?
+        if [ "$RET" -eq 0 ]; then
+            return
+        else
+            # 403 404 错误或达到重试次数
+            if [ "$RET" -eq 22 ] || [ "$i" -eq 5 ]; then
+                return "$RET"
+            fi
+            sleep 1
+        fi
+    done
+}
+
+check_root() {
     if [ "$EUID" -ne 0 ] || [ "$(id -ru)" -ne 0 ]; then
         die "This script must be run as root!"
     fi
+}
+
+check_bash() {
+    local BASH_VER
+
+    BASH_VER="$(bash --version | head -n1 | awk -F ' ' '{for (i=1; i<=NF; i++) if ($i ~ /^[0-9]+\.[0-9]+\.[0-9]+/) {print $i; exit}}' | cut -d . -f1)"
     if [ -z "$BASH_VERSION" ]; then
         die "This script needs to be run with bash, not sh!"
     fi
+    if [ -z "$BASH_VER" ] || ! [[ "$BASH_VER" =~ ^[0-9]+$ ]]; then
+        die "Failed to parse Bash version!"
+    fi
+    if [ "$BASH_VER" -lt 4 ]; then
+        die "Bash version is lower than 4.0!"
+    fi
+}
+
+check_session() {
     if [ -z "$SSH_CONNECTION" ] || [ -z "$(awk '{print $1}' <<< "$SSH_CONNECTION")" ]; then
         die "Failed to determine the client IP via SSH connection."
     fi
@@ -53,17 +83,17 @@ ip_info() {
     local USER_IP IP_API USER_REGION USER_CITY
 
     USER_IP="$(awk '{print $1}' <<< "$SSH_CONNECTION")"
-    IP_API="$(curl --user-agent "$UA_BROWSER" "${CURL_OPTS[@]}" -fsL "https://api.ipbase.com/v1/json/$USER_IP")" || \
-        IP_API="$(curl --user-agent "$UA_BROWSER" "${CURL_OPTS[@]}" -fsL "https://api.ip2location.io?ip=$USER_IP&format=json")" || \
-        IP_API="$(curl --user-agent "$UA_BROWSER" "${CURL_OPTS[@]}" -fsL "https://api.ip.sb/geoip/$USER_IP")" || \
-        IP_API="$(curl --user-agent "$UA_BROWSER" "${CURL_OPTS[@]}" -fsL "https://freeipapi.com/api/json/$USER_IP")" || \
+    IP_API="$(curl -Ls "https://api.ipbase.com/v1/json/$USER_IP")" || \
+        IP_API="$(curl -Ls "https://api.ip2location.io?ip=$USER_IP&format=json")" || \
+        IP_API="$(curl --user-agent Mozilla -Ls "https://api.ip.sb/geoip/$USER_IP")" || \
+        IP_API="$(curl -Ls "https://freeipapi.com/api/json/$USER_IP")" || \
     die "unable to obtain valid ip info, please check the network."
     USER_REGION="$(sed -En 's/.*"(region_name|regionName|region)":[ ]*"([^"]+)".*/\2/p' <<< "$IP_API")"
     USER_CITY="$(sed -En 's/.*"(city_name|cityName|city)":[ ]*"([^"]+)".*/\2/p' <<< "$IP_API")"
     echo "$USER_REGION" "$USER_CITY"
 }
 
-weather_run() {
+weather() {
     local USER_REGION USER_CITY
 
     read -r USER_REGION USER_CITY <<< "$(ip_info)"
@@ -71,13 +101,15 @@ weather_run() {
     echo
     echo "$(_yellow "Welcome! Users from") $(_cyan "${USER_REGION:-Unknown Region}")!"
     echo
-    curl "${CURL_OPTS[@]}" -fsL "https://wttr.in/$USER_CITY?1"
+    curl -Ls "https://wttr.in/$USER_CITY?1"
 }
 
-weather() {
-    clrscr
-    before_run
-    weather_run
+main() {
+    clear
+    check_root
+    check_bash
+    check_session
+    weather
 }
 
-weather
+main
